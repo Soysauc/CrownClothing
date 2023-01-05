d1eff90c7a8071333c1c2cd83cb4ffb26c8d653f
Jeremy M Rivera <fangman09@gmail.com>
2023-01-05 10:01:51 -0500

    further clean up on forms

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,7 +4,6 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import "./sign-up-form.styles.scss";
-import { UserContext } from "../../contexts/user.contexts";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
